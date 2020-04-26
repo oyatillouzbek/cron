6bf621116f533e6e00b6eafb49f0582debf2e5dc
oyatillouzbek <63519552+oyatillouzbek@users.noreply.github.com>
2020-04-26 10:01:03 +0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,9 +1,11 @@
 from flask import Flask
+import os
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+    os.system("python pyro.py")
 
 if __name__ == '__main__':
     app.run()
