50312a897c6cb47916029d7efc9736a9b64a6aeb
oyatillouzbek <63519552+oyatillouzbek@users.noreply.github.com>
2020-04-27 15:50:53 +0500

    Update pyro.py

--- pyro.py
+++ pyro.py
@@ -1,9 +1,23 @@
+import datetime
+import time
+import pytz
+from pyrogram import Client, Filters
 from pyrogram.api import functions
-from pyrogram import Client
+
 
 app_id = 1194939
 app_hash = "d8ed4b10ed554767a4570cf59c3ea49e"
 
 app = Client("my_account", api_id=app_id, api_hash=app_hash)
-app.run()
-app.send(functions.account.UpdateStatus(offline=False))
+tz = pytz.timezone('Asia/Tashkent')
+X=datetime.datetime.now(tz=tz)
+Y=X.strftime("%H:%M")
+while True:
+    app.start()
+    app.send(functions.account.UpdateProfile(first_name=str(Y),about="Soat: {}   Admin: Tilon".format(str(Y))))
+    time.sleep(60)
+
+while True:
+    app.send(functions.account.UpdateStatus(offline=False))
+    time.sleep(60)
+    
