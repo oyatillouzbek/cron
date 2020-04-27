669ca58958d2aff949c68e7b6e7652fa1b4fbca3
oyatillouzbek <63519552+oyatillouzbek@users.noreply.github.com>
2020-04-27 10:58:04 +0500

    Update pyro.py

--- pyro.py
+++ pyro.py
@@ -1,4 +1,5 @@
 import datetime
+import time
 from pyrogram import Client, Filters
 from pyrogram.api import functions
 
@@ -7,9 +8,8 @@ app_id = 1194939
 app_hash = "d8ed4b10ed554767a4570cf59c3ea49e"
 
 app = Client("my_account", api_id=app_id, api_hash=app_hash)
+app.start()
 
-@app.on_message(Filters.audio)
-def start(client,message):
     X=datetime.datetime.now()
     Y=X.strftime("%H:%M")
     while True:
@@ -17,5 +17,5 @@ def start(client,message):
             first_name=str(Y),
             about="Soat: {}           Admin: Tilon".format(str(Y))
             ))
-
-app.run()
+        time.sleep(60)
+app.stop()
