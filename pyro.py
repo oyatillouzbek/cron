9d85298aa0a06ea571d6c6ebb366da50bfb26398
oyatillouzbek <63519552+oyatillouzbek@users.noreply.github.com>
2020-04-27 11:32:34 +0500

    Update pyro.py

--- pyro.py
+++ pyro.py
@@ -1,5 +1,4 @@
 import datetime
-import time
 from pyrogram import Client, Filters
 from pyrogram.api import functions
 
@@ -8,12 +7,8 @@ app_id = 1194939
 app_hash = "d8ed4b10ed554767a4570cf59c3ea49e"
 
 app = Client("my_account", api_id=app_id, api_hash=app_hash)
-
 X=datetime.datetime.now()
 Y=X.strftime("%H:%M")
-app.send(functions.account.UpdateProfile(
-            first_name=str(Y),
-            about="Soat: {}           Admin: Tilon".format(str(Y))
-            ))
-time.sleep(5)
-app.run()
+while True:
+    app.start()
+    app.send(functions.account.UpdateProfile(first_name=str(Y),about="Soat: {}           Admin: Tilon".format(str(Y))))
