85d128460a5bc1f1fc3cac22c540429eb1bd2017
oyatillouzbek <63519552+oyatillouzbek@users.noreply.github.com>
2020-04-27 14:49:12 +0500

    Update pyro.py

--- pyro.py
+++ pyro.py
@@ -5,5 +5,5 @@ app_id = 1194939
 app_hash = "d8ed4b10ed554767a4570cf59c3ea49e"
 
 app = Client("my_account", api_id=app_id, api_hash=app_hash)
+app.start()
 app.send(functions.account.UpdateStatus(offline=False))
-app.run()
