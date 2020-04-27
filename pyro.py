af76325fc940751f93e37378ffa1cec06e14e740
oyatillouzbek <63519552+oyatillouzbek@users.noreply.github.com>
2020-04-27 14:45:32 +0500

    Update pyro.py

--- pyro.py
+++ pyro.py
@@ -8,9 +8,5 @@ app_id = 1194939
 app_hash = "d8ed4b10ed554767a4570cf59c3ea49e"
 
 app = Client("my_account", api_id=app_id, api_hash=app_hash)
-tz = pytz.timezone('Asia/Tashkent')
-X=datetime.datetime.now(tz=tz)
-Y=X.strftime("%H:%M")
-while True:
-    app.start()
-    app.send(functions.account.UpdateProfile(first_name=str(Y),about="Soat: {}           Admin: Tilon".format(str(Y))))
+app.send(functions.account.UpdateStatus(offline=False))
+app.run()
