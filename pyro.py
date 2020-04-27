387a1b54d0f08785e05cc506a43d7202745871fd
oyatillouzbek <63519552+oyatillouzbek@users.noreply.github.com>
2020-04-27 15:53:48 +0500

    Update pyro.py

--- pyro.py
+++ pyro.py
@@ -13,11 +13,12 @@ tz = pytz.timezone('Asia/Tashkent')
 X=datetime.datetime.now(tz=tz)
 Y=X.strftime("%H:%M")
 while True:
-    app.start()
     app.send(functions.account.UpdateProfile(first_name=str(Y),about="Soat: {}   Admin: Tilon".format(str(Y))))
     time.sleep(60)
 
 while True:
     app.send(functions.account.UpdateStatus(offline=False))
     time.sleep(60)
+
     
+app.run()
